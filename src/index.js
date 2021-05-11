c17395a99bec6d65f14a69a2d166c0ff61ccb06e
anilsa1400 <30488966+anilsa1400@users.noreply.github.com>
2021-05-11 13:25:29 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
+import React from "react";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+//   rootElement
+// );
+
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+  React.createElement("h1",null,"Hello!"),
+  document.getElementById("root")
+)
+
+
