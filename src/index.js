cf79be9f18bffdec3b0c18216abcd12e767723f7
anilsa1400 <30488966+anilsa1400@users.noreply.github.com>
2021-05-12 11:55:14 +0530

    useState feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import React from "react";
+import React, { useState } from "react";
 // import App from "./App";
 
 // Initial React element
@@ -142,21 +142,95 @@ let lakeList = [
   }
 ];
 
-function App(props) {
+// function App(props) {
+//   return (
+//     <div>
+//       {props.lakes.map((lake, index) => {
+//         return (
+//           <div key={index}>
+//             <h1>
+//               {lake.id}: {lake.name}{" "}
+//             </h1>
+//             <li>City: {lake.city}</li>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<App lakes={lakeList} />, document.getElementById("root"));
+
+function Lake({ name }) {
+  return <h1>Visit {name}</h1>;
+}
+
+function SkiResort({ name }) {
+  return <h1>Visit {name}</h1>;
+}
+
+// function App(props) {
+//   return (
+//     <div>
+//       {props.season === "summer" ? (
+//         <Lake name="Manali" />
+//       ) : props.season === "winter" ? (
+//         <SkiResort name="Sri lanka" />
+//       ) : (
+//         <h1>Come back in Winter or Summer!</h1>
+//       )}
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<App season="winter" />, document.getElementById("root"));
+
+/**
+ * To Render multiple adjacent components enclose components within  <React.Fragment> or just <> tags
+ * ERROR: SyntaxError: /src/index.js: Adjacent JSX elements must be wrapped in an enclosing tag. Did you want a JSX fragment <>...</>? (203:6)
+ */
+// function App(props) {
+//   return (
+//     <React.Fragment>
+//       <Lake name="Manali" />
+//       <SkiResort name="Sri lanka" />
+//     </React.Fragment>
+//   );
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+/**
+ * Array destructuring
+ */
+// const [first, , third] = ["Pineapple", "Pizza", "Kaboom"];
+// console.log(first);
+// console.log(third);
+
+function App() {
+  const [year, setYear] = useState(2021);
+  const [manager, setManager] = useState("Alex");
+  const [status, setStatus] = useState("Open");
   return (
-    <div>
-      {props.lakes.map((lake) => {
-        return (
-          <div>
-            <h1>
-              {lake.id}: {lake.name}{" "}
-            </h1>
-            <li>City: {lake.city}</li>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div>
+        <h1>New Year {year}</h1>
+        <button onClick={() => setYear(year + 1)}>Set Year</button>
+      </div>
+      <div>
+        <h1>Manager On Duty: {manager}</h1>
+        <button onClick={() => setManager("Racheal")}>Set Racheal</button>
+      </div>
+      <div>
+        <h1>Status:{status}</h1>
+        <button onClick={() => setStatus("Open")}>Open</button>
+        <button onClick={() => setStatus("Back in 5 mins")}>
+          Back in 5 mins
+        </button>
+        <button onClick={() => setStatus("Closed")}>Closed</button>
+      </div>
+    </>
   );
 }
 
-ReactDOM.render(<App lakes={lakeList} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
